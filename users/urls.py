ab68c2863d124f1a3bfee5f3efc5080ba29b5bf0
RohithGangarapu <gangarapurohith4@gmail.com>
2025-05-20 12:43:19 +0530

    changed url format for better navigation

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r'', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('auth/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/profile/', UserProfileUpdateView.as_view(), name='profile-update'),
+    path('register/', include(router.urls)),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('updateprofile/', UserProfileUpdateView.as_view(), name='profile-update'),
 ]
