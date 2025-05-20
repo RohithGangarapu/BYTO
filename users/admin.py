36b75435db4fb304822af2d37fed99793331d748
Ritik Kumar Sen <ritikk060@gmail.com>
2025-05-20 10:14:37 +0530

    remove user permission from admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import *
+from .models import CustomUser
 from django.contrib.auth.models import Group
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    exclude = ('user_permissions','groups',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.unregister(Group)
 # Register your models here.
