62ef3c4427f4eb7f6e978de67be4c1b31e93e018
RohithGangarapu <gangarapurohith4@gmail.com>
2025-05-20 09:55:31 +0530

    removed Groups from admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Group
 admin.site.register(CustomUser)
+admin.site.unregister(Group)
 # Register your models here.
