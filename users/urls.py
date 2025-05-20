340fe9183f18caec26eb0d78f8d0573d773bb70e
RohithGangarapu <gangarapurohith4@gmail.com>
2025-05-20 15:27:41 +0530

    Removed Router from UserViewSet

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,8 @@
-from rest_framework.routers import DefaultRouter
 from .views import *
-from django.urls import path, include
-
-router = DefaultRouter()
-router.register(r'', UserViewSet, basename='users')
+from django.urls import path
 
 urlpatterns = [
-    path('register/', include(router.urls)),
+    path('register/', UserViewSet.as_view({'post': 'create'}),name='user_view'),
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('updateprofile/', UserProfileUpdateView.as_view(), name='profile-update'),
 ]
